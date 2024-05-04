f4f034b0c0ee03ff4cc1a7620511e2522eedc271
Sk Shahriar Ahmed Raka <skshahriarahmedraka@gmail.com>
2024-05-05 00:18:45 +0600

    added

---
+++ go/standard_code_writting.go
@@ -0,0 +1,20 @@
+
+
+    https://github.com/bahlo/go-styleguide
+
+    https://github.com/golang/go/wiki/Style
+
+https://github.com/smallnest/go_best_practices
+
+
+ Go Code Review Comments
+
+https://github.com/golang/go/wiki/CodeReviewComments
+
+Effective Go
+
+https://golang.org/doc/effective_go.html 
+
+http://devs.cloudimmunity.com/gotchas-and-common-mistakes-in-go-golang/
+
+https://github.com/golang-standards/project-layout
